f57d4abd7f3a5689c7b7bca1bc9a09a24eab4517
MateusCarlos <mateuscarlos.ti@gmail.com>
2025-03-05 09:50:20 -0300

    atualiza paciente

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import re
 import bleach
+from datetime import datetime
+import pytz
 
 def validate_cpf(cpf: str) -> bool:
     """Valida o formato e dígitos verificadores do CPF"""
@@ -26,3 +28,33 @@ def sanitize_input(value: str, max_length=100) -> str:
     if len(cleaned) > max_length:
         raise ValueError(f"Campo excede o tamanho máximo de {max_length} caracteres")
     return cleaned
+
+def get_local_time(utc_dt, timezone_str):
+    """
+    Converte a data e hora UTC para o fuso horário local do usuário.
+    
+    :param utc_dt: datetime em UTC
+    :param timezone_str: string do fuso horário (ex: 'America/Sao_Paulo')
+    :return: datetime no fuso horário local
+    """
+    local_tz = pytz.timezone(timezone_str)
+    local_dt = utc_dt.astimezone(local_tz)
+    return local_dt
+
+
+import requests
+
+def get_user_timezone(ip_address):
+    """
+    Obtém o fuso horário do usuário com base no endereço IP.
+    
+    :param ip_address: Endereço IP do usuário
+    :return: string do fuso horário (ex: 'America/Sao_Paulo')
+    """
+    try:
+        response = requests.get(f'http://ip-api.com/json/{ip_address}')
+        response.raise_for_status()
+        data = response.json()
+        return data.get('timezone', 'UTC')
+    except requests.RequestException:
+        return 'UTC'
