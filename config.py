8356bd29f19ef7c7e6d41be4672c0b6ed68a04e2
MateusCarlos <mateuscarlos.ti@gmail.com>
2025-03-06 14:43:29 -0300

    acompanha

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{os.getenv('DB_USER', 'myuser')}:{os.getenv('DB_PASSWORD', 'mypassword')}"
-        f"@mysql/{os.getenv('DB_NAME', 'cuidar_plus_bd')}"
+        f"@mysql:3306/{os.getenv('DB_NAME', 'cuidar_plus_bd')}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SWAGGER = {
