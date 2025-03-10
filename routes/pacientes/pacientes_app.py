28ae43b2849e610e5a0befccd8e5835e0a6e2893
MateusCarlos <mateuscarlos.ti@gmail.com>
2025-03-09 21:46:11 -0300

    ajuste pacientes

--- routes/pacientes/pacientes_app.py
+++ routes/pacientes/pacientes_app.py
@@ -4,6 +4,7 @@ from .update_paciente import atualizar_paciente
 from .delete_paciente import excluir_paciente
 from .get_paciente import buscar_paciente
 from .get_all_pacientes import get_all_pacientes
+from .get_paciente_by_id import get_paciente_by_id
 
 pacientes_routes = Blueprint('pacientes_routes', __name__)
 
@@ -12,3 +13,4 @@ pacientes_routes.route('/api/atualizar_paciente/<cpf>', methods=['PUT'])(atualiz
 pacientes_routes.route('/api/excluir_paciente/<cpf>', methods=['DELETE'])(excluir_paciente)
 pacientes_routes.route('/api/buscar_paciente', methods=['GET'])(buscar_paciente)
 pacientes_routes.route('/api/exibe_pacientes', methods=['GET'])(get_all_pacientes)
+pacientes_routes.route('/api/paciente/<int:id>', methods=['GET'])(get_paciente_by_id)
