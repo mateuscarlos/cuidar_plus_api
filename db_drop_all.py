b8e03a8344ccf17946b01a4367d440d578f96adc
MateusCarlos <mateuscarlos.ti@gmail.com>
2025-04-02 09:37:43 -0300

    Adiciona arquivos de configuração do projeto e ignora arquivos desnecessários. Atualiza estado do workspace e define tarefas para o ambiente de desenvolvimento.

--- db_drop_all.py
+++ db_drop_all.py
@@ -11,6 +11,12 @@ with app.app_context():
         connection.execute(text("DROP TABLE IF EXISTS acompanhamentos"))
         connection.execute(text("DROP TABLE IF EXISTS usuario"))
         connection.execute(text("DROP TABLE IF EXISTS paciente"))
+        connection.execute(text("DROP TABLE IF EXISTS users"))
+        connection.execute(text("DROP TABLE IF EXISTS convenios"))
+        connection.execute(text("DROP TABLE IF EXISTS user"))
+        connection.execute(text("DROP TABLE IF EXISTS plano"))
+        connection.execute(text("DROP TABLE IF EXISTS tratamento"))
+
         
         # Habilitar verificações de chaves estrangeiras novamente
         connection.execute(text("SET FOREIGN_KEY_CHECKS=1"))
