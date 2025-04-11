1a40e9016fddf115318b6a59b37c625ffd2ae715
MateusCarlos <mateuscarlos.ti@gmail.com>
2025-04-11 09:10:09 -0300

    refactor config.py for consistency in string formatting and improve readability

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
+
 class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = (
@@ -7,28 +8,37 @@ class Config:
         f"@mysql:3306/{os.getenv('DB_NAME', 'cuidar_plus_bd')}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'sua_chave_secreta'
-    ENV = 'development'  # Altere para 'production' em produção
+    SECRET_KEY = "sua_chave_secreta"
+    ENV = "development"  # Altere para 'production' em produção
     SWAGGER = {
-        'title': 'API Cuidar+',
-        'uiversion': 3,
-        'specs': [
+        "title": "API Cuidar+",
+        "uiversion": 3,
+        "specs": [
             {
-                'endpoint': 'apispec_1',
-                'route': '/apispec_1.json',
-                'rule_filter': lambda rule: True, 
-                'model_filter': lambda tag: True, 
+                "endpoint": "apispec_1",
+                "route": "/apispec_1.json",
+                "rule_filter": lambda rule: True,
+                "model_filter": lambda tag: True,
             }
         ],
-        'static_url_path': '/flasgger_static',
-        'swagger_ui': True,
-        'specs_route': '/docs/'
+        "static_url_path": "/flasgger_static",
+        "swagger_ui": True,
+        "specs_route": "/docs/",
     }
     CORS_RESOURCES = {
         r"/*": {
-            "origins": ["http://localhost:4200", "http://127.0.0.1:4200", "http://localhost:5001", "http://127.0.0.1:5001", "http://localhost:5000", "http://127.0.0.1:5000"],  # Permitir apenas este domínio
+            "origins": [
+                "http://localhost:4200",
+                "http://127.0.0.1:4200",
+                "http://localhost:5001",
+                "http://127.0.0.1:5001",
+                "http://localhost:5000",
+                "http://127.0.0.1:5000",
+                "http://127.0.0.1:8080",
+                "http://localhost:8080",
+            ],  # Permitir apenas este domínio
             "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
             "allow_headers": ["Authorization", "Content-Type", "X-Test-Environment"],
-            "supports_credentials": True
+            "supports_credentials": True,
         }
     }
