b9754b1d148c706381660358121dd9f20393a92a
Mateus Carlos <mateuscarlos.ti@gmail.com>
2025-05-25 18:07:26 -0300

    Corrige a ordem de remoção das tabelas no script de exclusão do banco de dados

--- db_drop_all.py
+++ db_drop_all.py
@@ -8,14 +8,14 @@ with app.app_context():
         connection.execute(text("SET FOREIGN_KEY_CHECKS=0"))
         
         # Drop tabelas na ordem correta
-        connection.execute(text("DROP TABLE IF EXISTS acompanhamentos"))
-        connection.execute(text("DROP TABLE IF EXISTS usuario"))
-        connection.execute(text("DROP TABLE IF EXISTS paciente"))
-        connection.execute(text("DROP TABLE IF EXISTS users"))
+        connection.execute(text("DROP TABLE IF EXISTS acompanhamento"))
         connection.execute(text("DROP TABLE IF EXISTS convenio"))
-        connection.execute(text("DROP TABLE IF EXISTS user"))
+        connection.execute(text("DROP TABLE IF EXISTS funcoes"))
+        connection.execute(text("DROP TABLE IF EXISTS paciente"))
+        connection.execute(text("DROP TABLE IF EXISTS setores"))
         connection.execute(text("DROP TABLE IF EXISTS plano"))
-        connection.execute(text("DROP TABLE IF EXISTS tratamento"))
+        connection.execute(text("DROP TABLE IF EXISTS user"))
+        
 
         
         # Habilitar verificações de chaves estrangeiras novamente
